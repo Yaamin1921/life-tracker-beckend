6de1bb864682e489b49e018af20f4c90433868d0
Mohd Yaamin <yaamin1921413@gmail.com>
2026-01-29 00:14:56 +0530

    initial commit

---
+++ src/main/java/com/life/tracker/api/beckend/LifeTrackerApiBeckendApplication.java
@@ -0,0 +1,13 @@
+package com.life.tracker.api.beckend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeTrackerApiBeckendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeTrackerApiBeckendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/life/tracker/api/beckend/LifeTrackerApiBeckendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.life.tracker.api.beckend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeTrackerApiBeckendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
